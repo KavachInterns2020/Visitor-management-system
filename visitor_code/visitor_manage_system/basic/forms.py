ec2139bae0eca73fa5037e94308baa394edf872b
vbhv12 <48524857+vbhv12@users.noreply.github.com>
2020-07-16 18:02:10 +0530

    Update forms.py

--- visitor_code/visitor_manage_system/basic/forms.py
+++ visitor_code/visitor_manage_system/basic/forms.py
@@ -1,17 +1,28 @@
 from django.forms import ModelForm
+from django import forms
 from .models import *
+from django.core import validators
+
+def checkforz(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("please check")
 
 
-class HostForm(ModelForm):
+class HostForm(forms.ModelForm):
+    name=forms.CharField(validators=[checkforz])
     class Meta:
         model = Host
         fields = '__all__'
-    
-
+         
+  
+  
 class VisitorForm(ModelForm):
     class Meta:
         model = Visitor
         fields = '__all__'
+        
+        
+  
 
 class VisitDetailsForm(ModelForm):
     class Meta:
